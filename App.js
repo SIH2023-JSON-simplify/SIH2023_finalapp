27d6ac0088cb93efdc1ac585fec091403ce8d6d1
MacBook Pro <macbookpro@Vishesh.local>
2023-09-07 19:06:46 +0530

    reanimated stable version

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import 'react-native-gesture-handler'
 import React, { useEffect } from 'react';
-import { Text, View, disableYellowBox } from 'react-native';
-
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import "react-native-reanimated"
 import Navigation from './src/navigation/Navigation';
