65b20f3860f49cdc77daad001ceebf1a86a60663
MacBook Pro <macbookpro@Vishesh.local>
2023-09-09 00:04:11 +0530

    video call

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -8,6 +8,7 @@ import Verifyotp from '../screens/Verifyotp/Verifyotp';
 import TabNavigator from './TabNavigator';
 import Notification from '../screens/Notification/Notification';
 import Settings from '../screens/Settings/Settings';
+import VideoCall from '../screens/VideoCall/VideoCall';
 
 
 
@@ -20,13 +21,14 @@ function Navigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="Splash"
+                initialRouteName="VideoCall"
                 screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Splash" component={Splash} />
                 <Stack.Screen name="LoginLawyer" component={LoginLawyer} />
                 <Stack.Screen name="SignupLawyer" component={SignupLawyer} />
                 <Stack.Screen name="Verifyotp" component={Verifyotp} />
                 <Stack.Screen name="Notification" component={Notification} />
+                <Stack.Screen name="VideoCall" component={VideoCall} />
                 <Stack.Screen
                 name="Settings" component={Settings} />
                 <Stack.Screen 
